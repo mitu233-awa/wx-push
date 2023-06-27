7b2c22740019018bf840c5e21638aad62a206062
mitu233-awa <125428062+mitu233-awa@users.noreply.github.com>
2023-06-27 14:35:21 +0800

    index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   appid: "wx8ee126c3989a8718",
   appsecret: "9eb3f4e46879e5854ffd9ac705204af0",
-  template_id: "o9bhS6R91iKBfWWdlkyBUx3WbclM",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  template_id: "MK9Y0OPsIjjuj4N-o34PjsG8vm3coPWCEz0W9HIJZaA",
+  touser: "o9bhS6R91iKBfWWdlkyBUx3WbclM",
 
   key: "55e124311f262b739b01bf2b00662f0b",
   city: "青岛",
